c5b7dbc24551f504acad982dabc6913a8bd8183b
fun guy <39643236+AM4Ryzen@users.noreply.github.com>
2025-12-20 02:45:16 -0500

    Stupid shit that doesn't work
    Change-Id: Ic9e002b2d9e0922bcaf6ff3bd1fd1f42dbbd1f0b

--- edo.mk
+++ edo.mk
@@ -128,11 +128,11 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
-    android.hardware.audio.service \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.soundtrigger@2.3-impl
+    android.hardware.soundtrigger@2.1-impl
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
@@ -327,8 +327,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # HDR
 PRODUCT_COPY_FILES += \
