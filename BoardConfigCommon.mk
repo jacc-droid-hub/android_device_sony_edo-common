04f2e1985aa09bca5e498259ffe9ed81ba73f8cc
fun guy <39643236+AM4Ryzen@users.noreply.github.com>
2025-12-23 22:26:03 -0500

    Remove framework manifests
    Change-Id: Ie53b282808d42e01698822f3404d5fdb82350742

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -154,9 +154,6 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 #BOARD_HAS_QCA_FM_SOC := cherokee
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/framework_compatibility_matrix.xml
-DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
