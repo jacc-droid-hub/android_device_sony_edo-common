8b25cbcb77a384254519748dc1dc7c55758f67f1
fun guy <39643236+AM4Ryzen@users.noreply.github.com>
2025-12-30 02:55:36 -0500

    Make bin init scrips match Q
    Change-Id: If221f5e6100debd2f54db9b609f452cfb9c187dc

--- rootdir/bin/init_thermal.sh
+++ rootdir/bin/init_thermal.sh
@@ -2,6 +2,9 @@
 SENSOR_LIST="sdm_modem_skin_sa"
 
 MODEL=$(getprop ro.product.model)
+if [ "${MODEL}" != "XQ-AT72" -a "${MODEL}" != "XQ-AS72" ]; then
+	exit
+fi
 I=0;
 while true; do
 	if [ ! -d /sys/class/thermal/thermal_zone${I} ]; then
