1d55cc65e71eafb463c8cfa43a286c004b006b48
fun guy <39643236+AM4Ryzen@users.noreply.github.com>
2026-01-05 07:15:41 -0500

    Audio config changes part 1
    audio_policy_configuration.xml - Imported as is, this was nearly completely changed to build audio HAL, and stock Q matches stock S. https://github.com/LineageOS/android_device_sony_sm8250-common/commit/26b083392066b84ee3db57bc88c25113d22dc474
    
    audio/audio_platform_info.xml and qrd - Deleted https://github.com/LineageOS/android_device_sony_sm8250-common/commit/2fc32c0ca7620d6ddbf723c0050d6009a1b641f0
    
    audio/audio_platform_info_intcodec.xml - Overwrote because caf audio stuff, although something of note. SND_DEVICE_IN_SONY_GAMEENHANCER_HEADSET_MIC exists in Q, but not S. Not sure if important. Decided to keep removed here.
    
    Btw the bluetooth I committed was correct.
    https://github.com/LineageOS/android_device_sony_sm8250-common/commit/1d9989ef3d259b63574b1b68f5be7cce11c0a63c
    
    Change-Id: Id58a4c367b2fe3a33ed62447d3a4eda2aaf14367

--- edo.mk
+++ edo.mk
@@ -153,6 +153,7 @@ PRODUCT_PACKAGES += \
     libreverbwrapper \
     libsndmonitor \
     libspkrprot \
+    libssrec \
     libvisualizer \
     libvolumelistener
 
@@ -165,12 +166,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info_no_SVA.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info_no_SVA.xml \
-    $(LOCAL_PATH)/audio/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/bluetooth_qti_hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_qti_hearing_aid_audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/audio_configs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_configs.xml \
-    $(LOCAL_PATH)/audio/audio_platform_info_qrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_qrd.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
