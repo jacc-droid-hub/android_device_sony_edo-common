eb70390565e3a579f1dda3d9888d4039da949d87
fun guy <39643236+AM4Ryzen@users.noreply.github.com>
2026-01-05 07:58:33 -0500

    Drop contexthub
    tama did it, and I don't see anywhere where we would be building it.
    https://github.com/jacc-droid-hub/android_device_sony_tama-common/commit/984c452851ea96a11492775fd6886387deccd36a
    
    Change-Id: If8c73744b8b58bc6c5cd1dafb99e9cc9b1a0aafd

--- edo.mk
+++ edo.mk
@@ -257,11 +257,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
-# Context Hub
-PRODUCT_PACKAGES += \
-    android.hardware.contexthub@1.0-impl.generic \
-    android.hardware.contexthub@1.0-service
-
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
