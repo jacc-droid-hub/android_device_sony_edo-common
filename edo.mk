1b7e22fdc96bc9c44bb665e503cd0f0ba6d692e8
fun guy <39643236+AM4Ryzen@users.noreply.github.com>
2026-01-06 09:27:15 -0500

    NFC configs from lineage-20
    The NFC configs between 10 and 12 are near the same, so I'm importing the heavily modified lineage-20 configs as is.
    
    Change-Id: Ic2a2c9d1ac21b127634c09f4fbe87819f4be336a

--- edo.mk
+++ edo.mk
@@ -410,6 +410,7 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp-typef.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp-typef.conf \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
 
 PRODUCT_PACKAGES += \
