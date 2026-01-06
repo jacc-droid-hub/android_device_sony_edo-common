b428cef56c36589753648995297d178a6d75fea0
fun guy <39643236+AM4Ryzen@users.noreply.github.com>
2026-01-06 10:21:28 -0500

    Don't build audio amplifier anymore
    Speaker was working fine without it. Maybe could be made to build for 10 with a lot of knowledge, knowledge that I don't have.
    
    Change-Id: I187864d0668591bb2aa635f211be566831a6134a

--- edo.mk
+++ edo.mk
@@ -121,10 +121,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
-    tinyplay \
-    tinymix \
-    audio_amplifier.kona
+    android.hardware.soundtrigger@2.1-impl
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
