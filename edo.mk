0fca3dd5ff0de091481919997da2c70416866b10
fun guy <39643236+AM4Ryzen@users.noreply.github.com>
2026-01-15 12:51:13 -0500

    RIL might not be required in edo.mk
    Change-Id: I434b7f94d415d87b56e71e603b97d521addc3891

--- edo.mk
+++ edo.mk
@@ -256,6 +256,7 @@ PRODUCT_PACKAGES += \
     libdisplayconfig.qti.vendor \
     libqdMetaData \
     libtinyxml \
+    libxml2 \
     libvulkan \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
@@ -435,17 +436,6 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
-
-# RIL
-PRODUCT_PACKAGES += \
-    android.hardware.radio@1.4 \
-    android.hardware.radio.config@1.2 \
-    android.hardware.radio.deprecated@1.0 \
-    libprotobuf-cpp-full \
-    librmnetctl \
-    libxml2 \
-    extphonelib \
-    extphonelib.xml
 
 # Sensors
 PRODUCT_PACKAGES += \
