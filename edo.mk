4450fc90ac0c204576b46bcfcb084e540983934e
fun guy <39643236+AM4Ryzen@users.noreply.github.com>
2026-01-15 12:50:53 -0500

    Camera changes I am unsure about, but it brings them more inline with other devices
    Change-Id: I41721f93e925a1f5b40837e09269efd255b0aac6

--- edo.mk
+++ edo.mk
@@ -199,12 +199,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.common@1.0 \
     android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4 \
-    vendor.qti.hardware.camera.device@1.0.vendor \
-    vendor.qti.hardware.camera.postproc@1.0.vendor \
-    libcamera2ndk \
-    libcamera2ndk_vendor \
-    libgui \
-    libgui_vendor
+    vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # Using Sony Vendor Camera
 PRODUCT_PACKAGES += \
